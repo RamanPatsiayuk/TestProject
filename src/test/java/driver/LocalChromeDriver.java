b0dcdfeee1be96f797eff6129f6b6f787ec59ce7
Raman Patsiayuk <raman_patsiayuk@epam.com>
2018-04-04 15:27:47 +0300

    remove annotations

--- src/test/java/driver/LocalChromeDriver.java
+++ src/test/java/driver/LocalChromeDriver.java
@@ -1,6 +1,7 @@
 package driver;
 
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,6 +18,7 @@ public class LocalChromeDriver {
         System.out.println("Chrome driver");
         System.setProperty("webdriver.chrome.driver", filePath);
         driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1440, 900));
         /*ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--start-maximized");
         driver = new ChromeDriver(chromeOptions);*/
